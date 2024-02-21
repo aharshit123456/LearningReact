490796e79382e0045cfce38fa3381cb24154b679
Harshit Agarwal <aharshit123456@gmail.com>
2024-02-21 05:55:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LearningReact
+ A simple project for me to learn React Dom and React Native
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,12 @@
+
+// PascalCasing
+function Message() {
+    // JSK: Javasript XML
+    const name = "Harshit";
+    if(name)
+        return <h1>Hello {name}</h1> ;
+    return <h1>Hello World</h1> ;
+
+}
+
+export default Message;
